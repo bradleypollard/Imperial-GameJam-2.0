b303fa0e77a73c2cb1e081904d6ce6a2a43b9003
bradleypollard <bp912@ic.ac.uk>
2015-02-28 11:29:32 +0000

    11:30

---
+++ Assets/Scripts/Track.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Track : MonoBehaviour {
+
+    public Vector2 StartPoint;
+    public Vector2 EndPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
