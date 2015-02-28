4de09424308e9fc709794be06a584df3daf0193c
Zystral <zy.solaris@gmail.com>
2015-02-28 18:22:24 +0000

    DO YOU WANNA BUILD SOME TRA~ACKS?

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class GameLogic : MonoBehaviour {
 
     public Track[,] map = new Track[10,10];
-
+	public Camera c;
+	public GameObject track;
+	
 	// Use this for initialization
 	void Start () {
 	    foreach (GameObject g in GameObject.FindGameObjectsWithTag("Track"))
@@ -12,9 +14,14 @@ public class GameLogic : MonoBehaviour {
             map[(int)g.transform.position.x, (int)g.transform.position.y] = g.GetComponent<Track>();
         }
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetButtonDown("Fire1")){
+			Vector3 clickpos = c.ScreenToWorldPoint(Input.mousePosition);
+			Debug.Log("x is: " + clickpos.x + " and y is:" + clickpos.y);
+			GameObject newtrack = Instantiate(track, new Vector3(Mathf.Floor(clickpos.x), Mathf.Floor(clickpos.y), 0), Quaternion.identity) as GameObject;
+			//TODO: PUT DOWN SOME TRACK
+		}
 	}
 }
